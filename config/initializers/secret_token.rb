461be2e6bf9d1f1c060b9f796ecbf0439ab6f4b8
louis0526 <14301030@bjtu.edu.cn>
2019-01-07 19:16:07 +0800

    创建新项目测试

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knowscode::Application.config.secret_token = '899bdb7b86bc5bfb0e18a0aa022f03d0191e6add05b25659597c4f03dce2de378967884502ad98cce935318b5512a77700bdb09d93d5855091b5dab1b29c2ddc'
